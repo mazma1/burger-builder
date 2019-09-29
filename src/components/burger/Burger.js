4b0261517f392c3a68ff528400b4dc01031f88f2
Mary Mazi <mazi.mary.o@gmail.com>
2019-09-29 07:43:37 +0100

    manage burger ingredients in state

--- src/components/burger/Burger.js
+++ src/components/burger/Burger.js
@@ -4,11 +4,18 @@ import classes from 'components/burger/Burger.module.css';
 import BurgerIngredient from 'components/burger/ingredients/Ingredients';
 
 const Burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(ingredient => (
+      [...Array(props.ingredients[ingredient])].map((_, index) => (
+        <BurgerIngredient key={`ingredient${index}`} type={ingredient} />
+      ))
+    ));
+
+
   return (
     <div className={classes.burger}>
       <BurgerIngredient type="bread-top"/>
-      <BurgerIngredient type="cheese"/>
-      <BurgerIngredient type="meat"/>
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom"/>
     </div>
   );
