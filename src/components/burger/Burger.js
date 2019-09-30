88866a2a1d14c4f913b23676372f995628ea6879
Mary Mazi <mazi.mary.o@gmail.com>
2019-09-30 14:10:52 +0100

    return appropriate message if no ingredient is supplied

--- src/components/burger/Burger.js
+++ src/components/burger/Burger.js
@@ -4,13 +4,20 @@ import classes from 'components/burger/Burger.module.css';
 import BurgerIngredient from 'components/burger/ingredients/Ingredients';
 
 const Burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(ingredient => (
       [...Array(props.ingredients[ingredient])].map((_, index) => (
         <BurgerIngredient key={`ingredient${index}`} type={ingredient} />
       ))
-    ));
-
+    ))
+    .reduce(
+      (previousValue, item) => previousValue.concat(item),
+      [],
+    );
+    
+    if (transformedIngredients.length === 0) {
+      transformedIngredients = <p>Please start adding ingredients!</p>
+    }
 
   return (
     <div className={classes.burger}>
